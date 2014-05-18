0a21c9853f33ed1b220663660abae199d727496b
n-at <atnurgaliev@gmail.com>
2014-05-18 17:05:11 +0400

    Update logger for using winston.loggers

--- libs/logger.js
+++ libs/logger.js
@@ -7,22 +7,25 @@ var ENV = process.env.NODE_ENV;
 
 function getLogger(module) {
     var moduleName = module.filename.split(path.sep).slice(-2).join('/');
-    var logLevel = (ENV == 'development') ? 'debug' : 'info';
-    return new winston.Logger({
-        transports: [
-            new winston.transports.File({
+
+    if(!(moduleName in winston.loggers)) {
+        var logLevel = (ENV == 'development') ? 'debug' : 'info';
+        winston.loggers.add(moduleName, {
+            file: {
                 filename: path.join(config.get('log_path'), config.get('log_file')),
                 json: false,
                 level: logLevel,
                 label: moduleName
-            }),
-            new winston.transports.Console({
+            },
+            console: {
                 colorize: true,
                 level: logLevel,
                 label: moduleName
-            })
-        ]
-    });
+            }
+        });
+    }
+
+    return winston.loggers.get(moduleName);
 }
 
 module.exports = getLogger;
