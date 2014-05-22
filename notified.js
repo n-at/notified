ae30b0247e3d71c0b1addbf450bc4834487a5ea8
Alexey Nurgaliev <atnurgaliev@gmail.com>
2014-05-22 10:42:38 +0400

    "reload" command removed

--- notified.js
+++ notified.js
@@ -17,22 +17,6 @@ notification_template.load(function(err) {
     log.log('info', 'notified started');
 });
 
-//reload notified templates
-process.on('SIGNOTIFIEDRELOAD', function() {
-    log.info('Reloading templates...');
-    server.close(function() {
-        notification_template.load(function(err) {
-            if(err) {
-                log.error('Error occurred during reloading templates (%s)', err.message);
-                process.exit(13);
-                return;
-            }
-            server.listen(config.get('port'), config.get('host'));
-            log.info('notified reloaded');
-        });
-    });
-});
-
 process.on('uncaughtException', function(err) {
     log.error('notified crashed (%s)', err.message);
     throw err;
